8e8bc3dc7ecace43021a3d0ee94debe7cbba66a5
AlphaTechnic <juho3471@gmail.com>
2021-03-04 16:14:49 +0900

    study disjoint_sets

--- disjoint_sets.py
+++ disjoint_sets.py
@@ -8,4 +8,37 @@ input :
 """
 
 import sys
+
 sys.stdin = open("input.txt", "r")
+
+
+# 경로 압축 기법 적용
+def find_parent(x):
+    if x == parent[x]:
+        return parent[x]
+    parent[x] = find_parent(parent[x])
+    return parent[x]
+
+
+def union_parent(a, b):
+    group_a = find_parent(a)
+    group_b = find_parent(b)
+    if group_a < group_b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+V, E = map(int, input().split())
+parent = [i for i in range(V + 1)]
+
+for i in range(E):
+    a, b = map(int, input().split())
+    union_parent(a, b)
+
+print("각 원소가 속한 집합 : ")
+for i in range(1, V + 1):
+    print(find_parent(i), end=' ')
+print("\n부모 테이블 : ")
+for p in parent[1:]:
+    print(p, end=' ')
