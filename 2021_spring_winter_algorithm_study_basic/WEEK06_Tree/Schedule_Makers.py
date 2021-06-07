210a15a4bdafe8ce9326cc053eaa8a7ad39493f5
AlphaTechnic <alphatechnicist@gmail.com>
2021-06-07 23:59:27 +0900

    Init Schedule makers proj

---
+++ 2021_spring_winter_algorithm_study_basic/WEEK06_Tree/Schedule_Makers.py
@@ -0,0 +1,18 @@
+"""
+input :
+	  task의 cost, 일주일 간 널널한 정도, end date
+업무 1 :    50
+업무 2 :
+
+HFS2001-01
+HFS2002-01
+MAT3020-01
+"""
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+task_cost_end = dict()  # 빅데이터프로그래밍 50 2020-06-02
+time_spaces = []  # 100 100 80 20 100 100 100 20
+
+
