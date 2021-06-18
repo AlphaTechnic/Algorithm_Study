4dac45c90052ee9ea3be3b9ce911982ee80916da
AlphaTechnic <alphatechnicist@gmail.com>
2021-06-18 23:59:21 +0900

    Make some cases

--- 2021_spring_winter_algorithm_study_basic/WEEK06_Tree/Schedule_Makers.py
+++ 2021_spring_winter_algorithm_study_basic/WEEK06_Tree/Schedule_Makers.py
@@ -15,8 +15,13 @@ input = sys.stdin.readline
 task_cost_end = dict()  # 빅데이터프로그래밍 50 2020-06-02
 time_spaces = []  # 100 100 80 20 100 100 100 20
 
-task_cost_end['빅데이터프로그래밍'] = (50, 13)  # 13일 뒤
-task_cost_end['시스템프로그래밍'] = (100, 14)  # 14일 뒤
-task_cost_end['네트워크과제'] = (20, 7)  # 7일 뒤까지 가벼운 과제
+task_cost_end['빅데이터프로그래밍'] = [50, 13]  # 13일 뒤
+task_cost_end['시스템프로그래밍'] = [100, 14]  # 14일 뒤
+task_cost_end['네트워크과제'] = [20, 7]  # 7일 뒤까지 가벼운 과제
 
+for key in task_cost_end.keys():
+    cost = task_cost_end[key][0]
+    remaining_days = task_cost_end[key][1]
+    task_cost_end[key].append(cost / remaining_days)
 
+    print("%-10s : %s" % (key, task_cost_end[key][2]))
