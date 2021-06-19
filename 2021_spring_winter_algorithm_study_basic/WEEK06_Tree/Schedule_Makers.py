8c06612838bd4eaf8a297825c47d618216c46670
AlphaTechnic <alphatechnicist@gmail.com>
2021-06-19 23:48:47 +0900

    Generate the day schedule

--- 2021_spring_winter_algorithm_study_basic/WEEK06_Tree/Schedule_Makers.py
+++ 2021_spring_winter_algorithm_study_basic/WEEK06_Tree/Schedule_Makers.py
@@ -9,6 +9,7 @@ HFS2002-01
 MAT3020-01
 """
 import sys
+
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
@@ -19,9 +20,15 @@ task_cost_end['빅데이터프로그래밍'] = [50, 13]  # 13일 뒤
 task_cost_end['시스템프로그래밍'] = [100, 14]  # 14일 뒤
 task_cost_end['네트워크과제'] = [20, 7]  # 7일 뒤까지 가벼운 과제
 
+tot = 0
+time_space = 8
 for key in task_cost_end.keys():
     cost = task_cost_end[key][0]
     remaining_days = task_cost_end[key][1]
     task_cost_end[key].append(cost / remaining_days)
 
-    print("%-10s : %s" % (key, task_cost_end[key][2]))
+    tot += task_cost_end[key][2]
+
+for key in task_cost_end.keys():
+    print("%-10s : %.1f시간 정도를 써서 %.1f%%을 끝내야 함"
+          % (key, time_space * task_cost_end[key][2] / tot, task_cost_end[key][2]))
