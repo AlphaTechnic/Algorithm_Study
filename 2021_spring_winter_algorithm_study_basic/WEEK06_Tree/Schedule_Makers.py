f750271a1830ba9bbec21f7010ac31652d6c0335
AlphaTechnic <alphatechnicist@gmail.com>
2021-06-20 23:59:16 +0900

    Tidy up the plan

--- 2021_spring_winter_algorithm_study_basic/WEEK06_Tree/Schedule_Makers.py
+++ 2021_spring_winter_algorithm_study_basic/WEEK06_Tree/Schedule_Makers.py
@@ -1,34 +1,43 @@
 """
-input :
-	  task의 cost, 일주일 간 널널한 정도, end date
-업무 1 :    50
-업무 2 :
+목적 :
+여러 개의 작업 정보들(작업강도, 마감기한)이 있을 때, 오늘 나의 스케줄을 정리해주자.
 
-HFS2001-01
-HFS2002-01
-MAT3020-01
+input :
+작업 강도, 마감 기한, 투자 시간
+
+output :
+Today's Schedule
+
+우려사항 :
+1. 수익모델 모르겠음. (배너광고, 광고 차단, 스티커 아이템 판매)
+2. 가벼운 프로그램인 만큼 진입장벽 낮을 수 있음
+3. 프론트 역할이 절대적 반응형으로 한다면, 백엔드가 필요 없을 수도
 """
 import sys
 
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
-task_cost_end = dict()  # 빅데이터프로그래밍 50 2020-06-02
+JOBS = dict()  # 빅데이터프로그래밍 50 2020-06-02
 time_spaces = []  # 100 100 80 20 100 100 100 20
 
-task_cost_end['빅데이터프로그래밍'] = [50, 13]  # 13일 뒤
-task_cost_end['시스템프로그래밍'] = [100, 14]  # 14일 뒤
-task_cost_end['네트워크과제'] = [20, 7]  # 7일 뒤까지 가벼운 과제
+N = int(input())
+for _ in range(N):
+    job_name, d_day, intensity = input().split()
+    d_day, intensity = map(int, [d_day, intensity])
+    JOBS[job_name] = [d_day, intensity]
 
 tot = 0
 time_space = 8
-for key in task_cost_end.keys():
-    cost = task_cost_end[key][0]
-    remaining_days = task_cost_end[key][1]
-    task_cost_end[key].append(cost / remaining_days)
+for key in JOBS.keys():
+    d_day = JOBS[key][0]
+    cost = JOBS[key][1]
+    JOBS[key].append(cost / d_day)
 
-    tot += task_cost_end[key][2]
+    tot += JOBS[key][2]
 
-for key in task_cost_end.keys():
-    print("%-10s : %.1f시간 정도를 써서 %.1f%%을 끝내야 함"
-          % (key, time_space * task_cost_end[key][2] / tot, task_cost_end[key][2]))
+for key in JOBS.keys():
+    print("%-15s : %.1f시간, 전체 작업의 %.1f%%"
+          % (key, time_space * JOBS[key][2] / tot, JOBS[key][2]))
+
+# merge, split 등 제공
