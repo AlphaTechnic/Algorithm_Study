f9b775d5cb9f7e8d14770cb5a7123d8c9a1b5793
AlphaTechnic <alphatechnicist@gmail.com>
2021-07-13 14:39:38 +0900

    Solve a gcd problem

--- 2021_summer_algorithm_study_basic/using_cpp/1735.cpp
+++ 2021_summer_algorithm_study_basic/using_cpp/1735.cpp
@@ -1,4 +1,34 @@
-//
-// Created by 김주호 on 2021/07/13.
-//
+/*
+input :
+2 7
+3 5
 
+output :
+31 35
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// 전역 변수 선언
+int a, b, c, d;
+int p, q;
+
+int gcd(int a, int b){
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    cin >> a >> b >> c >> d;
+    q = a * d + b * c;
+    p = b * d;
+    int g = gcd(p, q);
+    cout << q / g << ' ' << p / g;
+
+    return 0 ;
+}
