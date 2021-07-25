884f920b9a7269a0e4201767fefdc48f9129f14f
AlphaTechnic <alphatechnicist@gmail.com>
2021-07-25 14:32:25 +0900

    Practice a extended GCD

---
+++ 2021_summer_algorithm_study_intermediate/using_python/14565.py
@@ -0,0 +1,35 @@
+"""
+input :
+26 11
+
+output :
+15 19
+"""
+
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+
+def xGCD(a, b):
+    x0, x1 = 1, 0
+    y0, y1 = 0, 1
+    while b != 0:
+        q = a // b
+        a, b = b, a % b
+        x1, x0 = x0 - q * x1, x1
+        y1, y0 = y0 - q * y1, y1
+
+    return a, x0, y0
+
+
+if __name__ == "__main__":
+    MOD, A = map(int, input().rstrip().split())
+
+    Ap_inv = (MOD - A + MOD) % MOD
+
+    Am_inv = -1
+    if xGCD(A, MOD)[0] != 1:
+        print(Ap_inv, -1)
+    else:
+        print(Ap_inv, (xGCD(A, MOD)[1] + MOD) % MOD)
