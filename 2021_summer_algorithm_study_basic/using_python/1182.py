cd7d04a6304262437962b172af455d111f6b9d7b
AlphaTechnic <alphatechnicist@gmail.com>
2021-07-27 14:42:44 +0900

    Solve a backtracking problem

---
+++ 2021_summer_algorithm_study_basic/using_python/1182.py
@@ -0,0 +1,40 @@
+"""
+input :
+5 0
+-7 -3 -2 5 8
+
+output :
+1
+"""
+
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+
+def dfs(dep, tot):
+    global cnt
+
+    if dep == N:
+        if tot == S:
+            cnt += 1
+        return
+
+    dfs(dep + 1, tot + nums[dep])
+    dfs(dep + 1, tot)
+
+
+if __name__ == "__main__":
+    N, S = map(int, input().rstrip().split())
+    nums = list(map(int, input().rstrip().split()))
+
+    cnt = 0
+    visited = [False for _ in range(N)]
+
+    dfs(0, 0)
+
+    # S가 0인 경우는 아무것도 안들어가는 경우도 카운트되어서 그걸 빼줘야함
+    if S == 0:
+        print(cnt - 1)
+    else:
+        print(cnt)
