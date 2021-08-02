c44a0119c12530f4231c1bba9bf3ce7559f12628
AlphaTechnic <alphatechnicist@gmail.com>
2021-08-02 17:44:40 +0900

    Solve a dp problem

--- 2021_summer_algorithm_study_basic/using_cpp/2293.cpp
+++ 2021_summer_algorithm_study_basic/using_cpp/2293.cpp
@@ -1,4 +1,36 @@
-//
-// Created by 김주호 on 2021/08/02.
-//
+/*
+input :
+3 10
+1
+2
+5
 
+output :
+10
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// 전역 변수 선언
+int dp[10001] = {1};
+int coin[101];
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    freopen("input.txt", "r", stdin);
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 1; i <=n; i++){
+        cin >> coin[i];
+        for (int j = coin[i]; j <= k; j++){
+            dp[j] += dp[j - coin[i]];
+        }
+    }
+    cout << dp[k] << '\n';
+
+    return 0;
+}
