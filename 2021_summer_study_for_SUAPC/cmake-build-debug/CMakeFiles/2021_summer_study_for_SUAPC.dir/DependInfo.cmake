7364cf582478980b78d02b11358248d5d8fc0214
AlphaTechnic <alphatechnicist@gmail.com>
2021-08-03 11:42:00 +0900

    Solve a greedy problem

---
+++ 2021_summer_study_for_SUAPC/cmake-build-debug/CMakeFiles/2021_summer_study_for_SUAPC.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kimjuho/Documents/Algorithm_Study/2021_summer_study_for_SUAPC/day1_20947_kim.cpp" "/Users/kimjuho/Documents/Algorithm_Study/2021_summer_study_for_SUAPC/cmake-build-debug/CMakeFiles/2021_summer_study_for_SUAPC.dir/day1_20947_kim.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 2021_summer_study_for_SUAPC/cmake-build-debug/CMakeFiles/2021_summer_study_for_SUAPC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2021_summer_study_for_SUAPC"
+  "2021_summer_study_for_SUAPC.pdb"
+  "CMakeFiles/2021_summer_study_for_SUAPC.dir/day1_20947_kim.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2021_summer_study_for_SUAPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
