3b46798fe5a85b156734217770e4b841092cfe96
AlphaTechnic <alphatechnicist@gmail.com>
2021-08-18 18:43:23 +0900

    Refine the codes

--- 2021_summer_algorithm_study_intermediate/using_python/17387.py
+++ 2021_summer_algorithm_study_intermediate/using_python/17387.py
@@ -19,26 +19,26 @@ def disjoint(a, b, c, d):
     return max(a, b) < min(c, d) or min(a, b) > max(c, d)
 
 
+def intersect(p1, p2, q1, q2):
+    a, b = p1; c, d = p2
+    e, f = q1; g, h = q2
+    P1P2 = [c - a, d - b]; P1Q1 = [e - a, f - b]; P1Q2 = [g - a, h - b]
+    Q1Q2 = [g - e, h - f]; Q1P1 = [a - e, b - f]; Q1P2 = [c - e, d - f]
+
+    ab2cd = ccw(P1P2, P1Q1) * ccw(P1P2, P1Q2)
+    cd2ab = ccw(Q1Q2, Q1P1) * ccw(Q1Q2, Q1P2)
+    if ab2cd == 0 and cd2ab == 0:
+        return not disjoint(a, c, e, g) and not disjoint(b, d, f, h)
+    return ab2cd <= 0 and cd2ab <= 0
+
+
 if __name__ == "__main__":
     a, b, c, d = map(int, input().rstrip().split())
     e, f, g, h = map(int, input().rstrip().split())
 
     A = [a, b]; B = [c, d]; C = [e, f]; D = [g, h]
 
-    AB = [c - a, d - b]; AC = [e - a, f - b]; AD = [g - a, h - b]
-    CA = [e - a, f - b]; CB = [e - c, f - d]; CD = [g - e, h - f]
-
-    res1 = ccw(AB, AC); res2 = ccw(AB, AD)
-    res3 = ccw(CD, CA); res4 = ccw(CD, CB)
-
-    if res1 * res2 == 0 and res3 * res4 == 0:
-        if not disjoint(A[0], B[0], C[0], D[0]) and not disjoint(A[1], B[1], C[1], D[1]):
-            print(1)
-        else:
-            print(0)
-        exit()
-
-    if res1 * res2 <= 0 and res3 * res4 <= 0:
+    if intersect(A, B, C, D):
         print(1)
     else:
         print(0)
