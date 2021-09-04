bdba1f2a1a7a4efdd080bb2ed9be4db2c72ab061
AlphaTechnic <alphatechnicist@gmail.com>
2021-09-04 17:46:32 +0900

    Solve a problem : top down dp, memoization

---
+++ 2021_random_set_self_practice/5557.py
@@ -0,0 +1,43 @@
+"""
+input :
+11
+8 3 2 4 8 7 2 4 0 8 8
+
+output :
+10
+"""
+
+import sys
+from collections import deque
+sys.stdin = open("input.txt", "r")
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+
+
+def recur(ind, tot):
+    global K
+    global cnt
+    if ind == N - 1:
+        if tot == K:
+            return 1
+        else:
+            return 0
+    if not 0 <= tot <= 20:
+        return 0
+    if (ind, tot) in dp:
+        return dp[(ind, tot)]
+
+    a = recur(ind + 1, tot + nums[ind])
+    b = recur(ind + 1, tot - nums[ind])
+    dp[(ind, tot)] = a + b
+    return dp[(ind, tot)]
+
+
+if __name__ == "__main__":
+    N = int(input())
+    nums = list(map(int, input().rstrip().split()))
+    K = nums[-1]
+    nums.pop()
+
+    dp = dict()
+    print(recur(1, nums[0]))
