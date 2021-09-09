7c38e0966c822074c51bd44d73f4ae0a52935795
AlphaTechnic <alphatechnicist@gmail.com>
2021-09-09 18:45:20 +0900

    Solve a problem : string

---
+++ 2021_random_set_self_practice/1013.py
@@ -0,0 +1,28 @@
+"""
+input :
+3
+10010111
+011000100110001
+0110001011001
+
+output :
+NO
+NO
+YES
+"""
+import re
+import sys
+
+if __name__ == "__main__":
+    T = int(sys.stdin.readline())
+    results = []
+
+    for _ in range(T):
+        text = sys.stdin.readline().rstrip()
+        p = re.compile('(100+1+|01)+')
+        m = p.fullmatch(text)
+
+        if m:
+            print("YES")
+        else:
+            print("NO")
