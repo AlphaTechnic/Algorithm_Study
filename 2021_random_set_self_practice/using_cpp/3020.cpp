bee214c39b9d58c6c283d3eaa86c080301f77dac
AlphaTechnic <alphatechnicist@gmail.com>
2021-11-08 08:00:32 +0900

    Solve a problem : accumulated sum

--- 2021_random_set_self_practice/using_cpp/3020.cpp
+++ 2021_random_set_self_practice/using_cpp/3020.cpp
@@ -1,4 +1,68 @@
-//
-// Created by 김주호 on 2021/11/05.
-//
+/*
+input :
+6 7
+1
+5
+3
+3
+5
+1
 
+output :
+2 3
+ */
+#include <iostream>
+#define SZH (500005)
+using namespace std;
+
+// 전역 변수 선언
+int N, H;
+int chk[SZH];
+int accum_tot[SZH];
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    freopen("input.txt", "r", stdin);
+    cin >> N >> H;
+    for (int i = 0; i < N; i += 2) {
+        int num;
+        cin >> num;
+        chk[0]++;
+        chk[num]--;
+
+        cin >> num;
+        chk[H - num]++;
+        chk[H]--;
+    }
+
+//    for (int i = 0; i <= H; i++) {
+//        cout << chk[i] << ' ';
+//    }
+//    cout << '\n';
+
+    int tot = 0;
+    for (int i = 0; i < H; i++) {
+        tot += chk[i];
+        accum_tot[i] = tot;
+    }
+
+//    for (int i = 0; i <= H; i++) {
+//        cout << accum_tot[i] << ' ';
+//    }
+//    cout << '\n';
+
+    int mnv = SZH;
+    int num = 0;
+    for (int i = 0; i < H; i++) {
+        if (accum_tot[i] < mnv) {
+            mnv = accum_tot[i];
+            num = 1;
+        }
+        else if (accum_tot[i] == mnv) {
+            num++;
+        }
+    }
+    cout << mnv << ' ' << num << '\n';
+
+    return 0;
+}
