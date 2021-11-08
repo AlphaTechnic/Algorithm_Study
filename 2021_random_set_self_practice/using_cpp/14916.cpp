cece9ab1f0e92b00a5705d55fe913fc098cd473c
AlphaTechnic <alphatechnicist@gmail.com>
2021-11-09 08:35:26 +0900

    Solve a problem : number theory

--- 2021_random_set_self_practice/using_cpp/14916.cpp
+++ 2021_random_set_self_practice/using_cpp/14916.cpp
@@ -1,4 +1,36 @@
-//
-// Created by 김주호 on 2021/11/08.
-//
+/*
+input :
+13
 
+output :
+5
+ */
+#include <iostream>
+
+using namespace std;
+
+// 전역 변수 선언
+int N;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+
+    cin >> N;
+    if (N == 1 || N == 3) {
+        cout << -1 << '\n';
+        return 0;
+    }
+
+    int cnt = 0;
+    while (N % 5 != 0) {
+        N -= 2;
+        cnt++;
+    }
+    cnt += (N / 5);
+    cout << cnt << '\n';
+
+    return 0;
+}
