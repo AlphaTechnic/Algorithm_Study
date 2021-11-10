f43618791e387e0d84441832a2083f55a1a8d37a
AlphaTechnic <alphatechnicist@gmail.com>
2021-11-10 20:49:05 +0900

    Solve a problem : dp

--- 2021_random_set_self_practice/using_cpp/14650.cpp
+++ 2021_random_set_self_practice/using_cpp/14650.cpp
@@ -1,4 +1,31 @@
-//
-// Created by 김주호 on 2021/11/09.
-//
+/*
+input :
+3
 
+output :
+6
+ */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// 전역 변수 선언
+int N;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    freopen("input.txt", "r", stdin);
+
+    cin >> N;
+    if (N == 1) {
+        cout << 0 << '\n';
+        return 0;
+    }
+
+    cout << 2 * pow(3, N - 2) << '\n';
+
+    return 0;
+}
