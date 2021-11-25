92c973ba324f81da7af32228009efbc80ff7e4a6
AlphaTechnic <alphatechnicist@gmail.com>
2021-11-25 17:08:29 +0900

    Solve a problem : implementation

---
+++ 2021_random_set_self_practice/using_java/SrcCodes/src/time_complexity.java
@@ -0,0 +1,34 @@
+/*
+input :
+30
+
+output :
+7
+*/
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class time_complexity {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println(countFive(N));
+    }
+
+    public static int countFive(int num) {
+        int tot = 0;
+        int numSave = num;
+        int p = 5;
+        while (p < numSave) {
+            num = numSave;
+            tot += num / p;
+            p *= 5;
+        }
+        return tot;
+    }
+}
