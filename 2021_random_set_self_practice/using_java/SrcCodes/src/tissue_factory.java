675f93525bb4b0756ccf7300eb6537a0c7afe3cf
AlphaTechnic <alphatechnicist@gmail.com>
2021-11-25 17:06:41 +0900

    Solve a problem : number theory

---
+++ 2021_random_set_self_practice/using_java/SrcCodes/src/tissue_factory.java
@@ -0,0 +1,47 @@
+/*
+input :
+5 20
+4 3 7 1 6
+
+output :
+8
+ */
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class tissue_factory {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        int mxv = -1;
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            nums.add(num);
+            mxv = Math.max(mxv, num);
+        }
+
+        ArrayList<Integer> diff = new ArrayList<>();
+        long diffTot = 0l;
+        for (int i = 0; i < N; i++) {
+            diffTot += mxv - nums.get(i);
+            diff.add(mxv - nums.get(i));
+        }
+//        System.out.println(diff);
+//        System.out.println(diffTot);
+
+        long remain = M - diffTot;
+        if (remain < 0) {
+            System.out.println("No way!");
+        }
+        else {
+            System.out.println(mxv + remain / N);
+        }
+    }
+}
