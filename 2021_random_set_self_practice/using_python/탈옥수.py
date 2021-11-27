e7c6b0cc6341768da7689ffc18eab38aa9fbfd1c
AlphaTechnic <alphatechnicist@gmail.com>
2021-11-27 23:49:12 +0900

    Solve a problem : ccw

---
+++ 2021_random_set_self_practice/using_python/탈옥수.py
@@ -0,0 +1,56 @@
+"""
+input :
+5
+0.000 0.000
+1.000 1.000
+2.000 0.000
+3.000 1.500
+2.235 2.483
+
+output :
+RIGHT
+LEFT
+LEFT
+"""
+import sys
+
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+
+class Prisoner(object):
+    def __init__(self):
+        self.N = -1
+        self.seq = []
+
+    def input(self):
+        self.N = int(input())
+        for _ in range(self.N):
+            x, y = map(float, input().rstrip().split())
+            self.seq.append((x, y))
+
+    def ccw(self, p1, p2, p3):
+        ret = (p2[0] - p1[0]) * (p3[1] - p2[1]) - (p2[1] - p1[1]) * (p3[0] - p2[0])
+        if ret > 0:
+            return 1
+        elif ret < 0:
+            return -1
+        else:
+            return 0
+
+    def go(self):
+        idx = 0
+        while idx != len(self.seq) - 2:
+            if self.ccw(self.seq[idx], self.seq[idx + 1], self.seq[idx + 2]) == 1:
+                print("LEFT")
+            elif self.ccw(self.seq[idx], self.seq[idx + 1], self.seq[idx + 2]) == -1:
+                print("RIGHT")
+            else:
+                print("error")
+            idx += 1
+
+
+if __name__ == "__main__":
+    prisoner = Prisoner()
+    prisoner.input()
+    prisoner.go()
