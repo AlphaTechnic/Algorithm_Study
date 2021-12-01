a4bee084f77ceefeb7bae659f68d0a3be761f670
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-01 09:10:29 +0900

    Solve a problem : xor

--- 2021_random_set_self_practice/using_cpp/새로운암호화.cpp
+++ 2021_random_set_self_practice/using_cpp/새로운암호화.cpp
@@ -1,4 +1,41 @@
-//
-// Created by 김주호 on 2021/11/30.
-//
+/*
+input :
+2 4
 
+output :
+5
+ */
+
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+ll cum_xor(ll number) {
+    // 0 ~ number까지 cum_xor
+    ll cum_xor;
+    ll grp_num;
+    if ((number & 1) == 1) {
+        grp_num = (number + 1) / 2;
+        cum_xor = ((grp_num & 1) == 1) ? 1 : 0;
+    }
+    else {
+        grp_num = number / 2;
+        cum_xor = ((grp_num & 1) == 1) ? 1 ^ number : 0 ^ number;
+    }
+    return cum_xor;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    freopen("input.txt", "r", stdin);
+
+    ll a, b;
+    cin >> a >> b;
+
+    ll res = cum_xor(b) ^ cum_xor(a - 1);
+    cout << res << '\n';
+
+    return 0;
+}
