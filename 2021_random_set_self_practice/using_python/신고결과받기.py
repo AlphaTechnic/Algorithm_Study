fd506c15b5968e5808eb6f71e8edab3ffd90b1ff
AlphaTechnic <alphatechnicist@gmail.com>
2022-04-03 13:16:25 +0900

    [feat] solve 신고결과받기 - hash, set

--- 2021_random_set_self_practice/using_python/신고결과받기.py
+++ 2021_random_set_self_practice/using_python/신고결과받기.py
@@ -1,32 +1,58 @@
+from collections import defaultdict
+
+
 class User(object):
-    def __init__(self, name, report=None):
-        if report is None:
-            report = set()
+    def __init__(self, name, reporting=None):
+        if reporting is None:
+            reporting = set()
         self.name = name
-        self.report = report
+        self.reporting = reporting
+
+    def __repr__(self):
+        return f"name-{self.name}"
 
 
 class Pool(object):
-    def __init__(self, users, reports=None):
-        if reports is None:
-            reports = dict()
+    def __init__(self, users, threshold, id_list, reported=None):
+        if reported is None:
+            reported = defaultdict(set)
+        self.id_list = id_list
         self.users = users
-        self.reports = reports
+        self.threshold = threshold
+        self.reported = reported
+        self.punished = set()
+
+    def mk_punished_users(self):
+        for name, users_reporting in self.reported.items():
+            if len(self.reported[name]) >= self.threshold:
+                self.punished.add(name)
+
+    def mk_mail(self):
+        ret = []
+        for id in self.id_list:
+            cnt = 0
+            for bad in self.punished:
+                if bad in self.users[id].reporting:
+                    cnt += 1
+            ret.append(cnt)
+
+        return ret
 
 
 def solution(id_list, report, k):
     users = dict()
     for id in id_list:
         users[id] = User(id)
-    pools = Pool(users=set(id_list))
+
+    pools = Pool(users=users, threshold=k, id_list=id_list)
     for info in report:
         a, b = info.split()
-        users[a].report.add(b)
-        pools.reports[a] = b
-
-    return answer
+        users[a].reporting.add(b)
+        pools.reported[b].add(a)
+    pools.mk_punished_users()
+    return pools.mk_mail()
 
 
 if __name__ == "__main__":
     print(solution(["muzi", "frodo", "apeach", "neo"],
-                   ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"]))
+                   ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2))
